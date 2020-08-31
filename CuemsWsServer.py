cd5a6be5498050d26bf100db633a57c485147131
Ion Reguera <ion@stagelab.net>
2020-08-31 18:31:27 +0200

    Cleaning -debug

--- CuemsWsServer.py
+++ CuemsWsServer.py
@@ -17,12 +17,11 @@ logging.basicConfig(format='Cuems:ws-server: (%(threadName)-9s)-(%(funcName)s) %
 class CuemsWsServer:
     state = {"value": 0}
     users = dict()
-    projects = None
+    projects=[{"CuemsScript": {"uuid": "76861217-2d40-47a2-bdb5-8f9c91293855", "name": "Proyecto test 0", "date": "14/08/2020 11:18:16", "timecode_cuelist": {"CueList": {"Cue": [{"uuid": "bf2d217f-881d-47c1-9ad1-f5999769bcc5", "time": {"CTimecode": "00:00:33:00"}, "type": "mtc", "loop": "False", "outputs": {"CueOutputs": {"id": 5, "bla": "ble"}}}, {"uuid": "8ace53f3-74f5-4195-822e-93c12fdf3725", "time": {"NoneType": "None"}, "type": "floating", "loop": "False", "outputs": {"CueOutputs": {"physiscal": 1, "virtual": 3}}}], "AudioCue": {"uuid": "be288e38-887a-446f-8cbf-c16c9ec6724a", "time": {"CTimecode": "00:00:45:00"}, "type": "virtual", "loop": "True", "outputs": {"AudioCueOutputs": {"stereo": 1}}}}}, "floating_cuelist": {"CueList": {"DmxCue": {"uuid": "f36fa4b3-e220-4d75-bff1-210e14655c11", "time": {"CTimecode": "00:00:23:00"}, "dmx_scene": {"DmxScene": {"DmxUniverse": [{"id": 0, "DmxChannel": [{"id": 0, "&": 10}, {"id": 1, "&": 50}]}, {"id": 1, "DmxChannel": [{"id": 20, "&": 23}, {"id": 21, "&": 255}]}, {"id": 2, "DmxChannel": [{"id": 5, "&": 10}, {"id": 6, "&": 23}, {"id": 7, "&": 125}, {"id": 8, "&": 200}]}]}}, "outputs": {"DmxCueOutputs": {"universe0": 3}}}, "Cue": {"uuid": "17376d8f-84c6-4f28-859a-a01260a1dadb", "time": {"CTimecode": "00:00:05:00"}, "type": "virtual", "loop": "False", "outputs": {"CueOutputs": {"id": 3}}}}}}}, {"CuemsScript": {"uuid": "e05de59a-b281-4abf-83ba-97198d661a63", "name": "Segundo proyecto", "date": "13/08/2020 07:23:12", "timecode_cuelist": {"CueList": {"Cue": [{"uuid": "d47a75e2-f76e-4c77-b33e-e1df40ffdf02", "time": {"CTimecode": "00:00:33:00"}, "type": "mtc", "loop": "False", "outputs": {"CueOutputs": {"id": 5, "bla": "ble"}}}, {"uuid": "b5c35e3d-91f6-42d8-9825-0176354b44c1", "time": {"NoneType": "None"}, "type": "floating", "loop": "False", "outputs": {"CueOutputs": {"physiscal": 1, "virtual": 3}}}], "AudioCue": {"uuid": "aef5e289-03b0-4b39-99cd-90063d9b8c80", "time": {"CTimecode": "00:00:45:00"}, "type": "virtual", "loop": "True", "outputs": {"AudioCueOutputs": {"stereo": 1}}}}}, "floating_cuelist": {"CueList": {"DmxCue": {"uuid": "5d4ef443-5a49-4986-a283-9563ee7a9e85", "time": {"CTimecode": "00:00:23:00"}, "dmx_scene": {"DmxScene": {"DmxUniverse": [{"id": 0, "DmxChannel": [{"id": 0, "&": 10}, {"id": 1, "&": 50}]}, {"id": 1, "DmxChannel": [{"id": 20, "&": 23}, {"id": 21, "&": 255}]}, {"id": 2, "DmxChannel": [{"id": 5, "&": 10}, {"id": 6, "&": 23}, {"id": 7, "&": 125}, {"id": 8, "&": 200}]}]}}, "outputs": {"DmxCueOutputs": {"universe0": 3}}}, "Cue": {"uuid": "37f80125-1c41-4cce-aab1-13328dd8c94e", "time": {"CTimecode": "00:00:05:00"}, "type": "virtual", "loop": "False", "outputs": {"CueOutputs": {"id": 3}}}}}}}]
 
     def __init__(self):
         
         self.event_loop = asyncio.get_event_loop()
-
 
         
 
@@ -73,18 +72,10 @@ class CuemsWsServer:
     async def notify_others(self, calling_user, type):
         print('notifing others')
         if self.users:  #notify others, not the user trigering the action, and only if the have same project loaded
-            print('calling user {}'.format(calling_user))
-            
             message = self.users_event(type)
-            print(self.users)
             for user, project in self.users.items():
-                print('user {}'.format(user))
-                print('project {}'.format(project))
                 if user is not calling_user:
-                    print('other user')
-                    print(self.users[calling_user])
                     if str(project) == str(self.users[calling_user]):
-                        
                         print('same project loaded')
                         await user.outgoing.put(message)
                         print('notifing {}'.format(user))
@@ -107,6 +98,8 @@ class CuemsWsServer:
         elif type == "changes":
             return json.dumps({"type": "state", "value" : "project modified in server"})
 
+    
+
 
 class CuemsWsUser(CuemsWsServer):
     def __init__(self, websocket, path):
@@ -114,8 +107,10 @@ class CuemsWsUser(CuemsWsServer):
         self.path = path
         self.incoming = asyncio.Queue()
         self.outgoing = asyncio.Queue()
+        self.users[self] = None
         super().__init__()
-        print(self.state)
+
+        
 
         
 
@@ -149,6 +144,7 @@ class CuemsWsUser(CuemsWsServer):
                 await self.list_projects()
             else:
                 logging.error("unsupported event: {}".format(data))
+                await self.notify_error_to_user("unsupported event: {}".format(data))
 
     async def producer(self):
         while True:
@@ -159,14 +155,21 @@ class CuemsWsUser(CuemsWsServer):
     async def notify_user(self, msg):
         await self.outgoing.put(json.dumps({"type": "state", "value":msg}))
 
+    async def notify_error_to_user(self, msg):
+        await self.outgoing.put(json.dumps({"type": "error", "value":msg}))
+
+
     async def list_projects(self):
-        
-        project_list = await self.load_project_list()    
-        await self.outgoing.put(json.dumps({"type": "list", "value": project_list}))
+        try:
+            project_list = await self.load_project_list()    
+            await self.outgoing.put(json.dumps({"type": "list", "value": project_list}))
+        except Exception as e:
+            print("error loading project list")
+            print("error: {} {}".format(type(e), e))
+            await self.notify_error_to_user('error loading project list')
 
     async def send_project(self, project_uuid):
         try:
-            print(project_uuid)
             if project_uuid == '':
                 raise NameError
             logging.info("user {} loading project {}".format(id(self.websocket), project_uuid))
@@ -175,50 +178,57 @@ class CuemsWsUser(CuemsWsServer):
             await self.outgoing.put(msg)
             await self.notify_user("project loaded")
             self.users[self] = project_uuid
-        except:
+        except Exception as e:
             print("error loading project")
+            print("error: {} {}".format(type(e), e))
+            await self.notify_error_to_user('error loading project')
 
     async def received_project(self, data):
         try:
             logging.info("user {} saving project {} : {}".format(id(self.websocket), self.users[self], data))
-            if (await self.save_project(self.users[self], json.loads(data))):
-                await self.notify_user("project saved")
-                await self.notify_others(self, "changes")
-        except:
+            project = json.loads(data)
+            project_uuid = project['CuemsScript']['uuid']
+            return_message = await self.save_project(project_uuid, project)
+            self.users[self] = project_uuid
+            await self.notify_user("{} project saved".format(return_message))
+            await self.notify_others(self, "changes")
+        except Exception as e:
             print("error saving project")
+            print("error: {} {}".format(type(e), e))
+            await self.notify_error_to_user('error saving project')
 
 
 
     @sync_to_async # call blocking function asynchronously (gets a thread)
     def load_project_list(self):
         logging.info("loading project list")
-        time.sleep(8)
-        
-        self.projects=[{"CuemsScript": {"uuid": "76861217-2d40-47a2-bdb5-8f9c91293855", "name": "Proyecto test 0", "date": "14/08/2020 11:18:16", "timecode_cuelist": {"CueList": {"Cue": [{"uuid": "bf2d217f-881d-47c1-9ad1-f5999769bcc5", "time": {"CTimecode": "00:00:33:00"}, "type": "mtc", "loop": "False", "outputs": {"CueOutputs": {"id": 5, "bla": "ble"}}}, {"uuid": "8ace53f3-74f5-4195-822e-93c12fdf3725", "time": {"NoneType": "None"}, "type": "floating", "loop": "False", "outputs": {"CueOutputs": {"physiscal": 1, "virtual": 3}}}], "AudioCue": {"uuid": "be288e38-887a-446f-8cbf-c16c9ec6724a", "time": {"CTimecode": "00:00:45:00"}, "type": "virtual", "loop": "True", "outputs": {"AudioCueOutputs": {"stereo": 1}}}}}, "floating_cuelist": {"CueList": {"DmxCue": {"uuid": "f36fa4b3-e220-4d75-bff1-210e14655c11", "time": {"CTimecode": "00:00:23:00"}, "dmx_scene": {"DmxScene": {"DmxUniverse": [{"id": 0, "DmxChannel": [{"id": 0, "&": 10}, {"id": 1, "&": 50}]}, {"id": 1, "DmxChannel": [{"id": 20, "&": 23}, {"id": 21, "&": 255}]}, {"id": 2, "DmxChannel": [{"id": 5, "&": 10}, {"id": 6, "&": 23}, {"id": 7, "&": 125}, {"id": 8, "&": 200}]}]}}, "outputs": {"DmxCueOutputs": {"universe0": 3}}}, "Cue": {"uuid": "17376d8f-84c6-4f28-859a-a01260a1dadb", "time": {"CTimecode": "00:00:05:00"}, "type": "virtual", "loop": "False", "outputs": {"CueOutputs": {"id": 3}}}}}}}, {"CuemsScript": {"uuid": "e05de59a-b281-4abf-83ba-97198d661a63", "name": "Segundo proyecto", "date": "13/08/2020 07:23:12", "timecode_cuelist": {"CueList": {"Cue": [{"uuid": "d47a75e2-f76e-4c77-b33e-e1df40ffdf02", "time": {"CTimecode": "00:00:33:00"}, "type": "mtc", "loop": "False", "outputs": {"CueOutputs": {"id": 5, "bla": "ble"}}}, {"uuid": "b5c35e3d-91f6-42d8-9825-0176354b44c1", "time": {"NoneType": "None"}, "type": "floating", "loop": "False", "outputs": {"CueOutputs": {"physiscal": 1, "virtual": 3}}}], "AudioCue": {"uuid": "aef5e289-03b0-4b39-99cd-90063d9b8c80", "time": {"CTimecode": "00:00:45:00"}, "type": "virtual", "loop": "True", "outputs": {"AudioCueOutputs": {"stereo": 1}}}}}, "floating_cuelist": {"CueList": {"DmxCue": {"uuid": "5d4ef443-5a49-4986-a283-9563ee7a9e85", "time": {"CTimecode": "00:00:23:00"}, "dmx_scene": {"DmxScene": {"DmxUniverse": [{"id": 0, "DmxChannel": [{"id": 0, "&": 10}, {"id": 1, "&": 50}]}, {"id": 1, "DmxChannel": [{"id": 20, "&": 23}, {"id": 21, "&": 255}]}, {"id": 2, "DmxChannel": [{"id": 5, "&": 10}, {"id": 6, "&": 23}, {"id": 7, "&": 125}, {"id": 8, "&": 200}]}]}}, "outputs": {"DmxCueOutputs": {"universe0": 3}}}, "Cue": {"uuid": "37f80125-1c41-4cce-aab1-13328dd8c94e", "time": {"CTimecode": "00:00:05:00"}, "type": "virtual", "loop": "False", "outputs": {"CueOutputs": {"id": 3}}}}}}}]
         project_list = list()
         for project in self.projects:
-            project_list.append({project['CuemsScript']['uuid']:{"name":project['CuemsScript']['name'], "date":project['CuemsScript']['date']}})
-        
+            try:
+                project_list.append({project['CuemsScript']['uuid']:{"name":project['CuemsScript']['name'], "date":project['CuemsScript']['date']}})
+            except:
+                print('malformed project')
         return project_list
 
     @sync_to_async # call blocking function asynchronously (gets a thread)
     def load_project(self, project):
         logging.info("loading project: {}".format(project))
-        time.sleep(5)
-        ret = None
         for elem in self.projects:
-            if elem['CuemsScript']['uuid'] == project:
-                ret = elem
-        return ret
+            if elem['CuemsScript']['uuid'] == project: 
+                return elem
+            else:
+                raise NameError
 
     @sync_to_async
-    def save_project(self, project, data):
-
-        time.sleep(6.6)
+    def save_project(self, uuid, data):
+        logging.info('saving project, uuid:{}, data:{}'.format(uuid, data))
         for num, elem in enumerate(self.projects):
-            if elem['CuemsScript']['uuid'] == project:
+            if elem['CuemsScript']['uuid'] == uuid:
                 self.projects[num] = data
-                return True
+                return 'updated'
+
+        self.projects.append(data)
+        return 'new'
 
 
 
