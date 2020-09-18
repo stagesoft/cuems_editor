16f03659d42d706e7d0921309cb5993897c70e96
Ion Reguera <ion@stagelab.net>
2020-09-18 20:35:24 +0200

    Fix class hierarchy

--- CuemsWsServer.py
+++ CuemsWsServer.py
@@ -12,23 +12,33 @@ from random import randint
 
 import time
 
-logging.basicConfig(format='Cuems:ws-server: (PID: %(process)d)-%(threadName)-9s)-(%(funcName)s) %(message)s', level=logging.INFO)
 
+
+
+stream = logging.StreamHandler()
+formatter = logging.Formatter('Cuems:ws-server: (PID: %(process)d)-%(threadName)-9s)-(%(funcName)s) %(message)s')
+stream.setFormatter(formatter)
+
+logger_ws_server = logging.getLogger()
+logger_ws_server.setLevel(logging.INFO)
+logger_ws_server.addHandler(stream)
+
+logger_asyncio = logging.getLogger('asyncio')
+logger_asyncio.setLevel(logging.INFO)  # asyncio debug level 
+
+logger_ws = logging.getLogger('websockets')
+logger_ws.setLevel(logging.INFO)  # websockets debug level,  in debug prints all frames, also binary frames! 
 
 class CuemsWsServer():
+    
     state = {"value": 0} #TODO: provisional
     users = dict()
     projects=[{"CuemsScript": {"uuid": "76861217-2d40-47a2-bdb5-8f9c91293855", "name": "Proyecto test 0", "date": "14/08/2020 11:18:16", "timecode_cuelist": {"CueList": {"Cue": [{"uuid": "bf2d217f-881d-47c1-9ad1-f5999769bcc5", "time": {"CTimecode": "00:00:33:00"}, "type": "mtc", "loop": "False", "outputs": {"CueOutputs": {"id": 5, "bla": "ble"}}}, {"uuid": "8ace53f3-74f5-4195-822e-93c12fdf3725", "time": {"NoneType": "None"}, "type": "floating", "loop": "False", "outputs": {"CueOutputs": {"physiscal": 1, "virtual": 3}}}], "AudioCue": {"uuid": "be288e38-887a-446f-8cbf-c16c9ec6724a", "time": {"CTimecode": "00:00:45:00"}, "type": "virtual", "loop": "True", "outputs": {"AudioCueOutputs": {"stereo": 1}}}}}, "floating_cuelist": {"CueList": {"DmxCue": {"uuid": "f36fa4b3-e220-4d75-bff1-210e14655c11", "time": {"CTimecode": "00:00:23:00"}, "dmx_scene": {"DmxScene": {"DmxUniverse": [{"id": 0, "DmxChannel": [{"id": 0, "&": 10}, {"id": 1, "&": 50}]}, {"id": 1, "DmxChannel": [{"id": 20, "&": 23}, {"id": 21, "&": 255}]}, {"id": 2, "DmxChannel": [{"id": 5, "&": 10}, {"id": 6, "&": 23}, {"id": 7, "&": 125}, {"id": 8, "&": 200}]}]}}, "outputs": {"DmxCueOutputs": {"universe0": 3}}}, "Cue": {"uuid": "17376d8f-84c6-4f28-859a-a01260a1dadb", "time": {"CTimecode": "00:00:05:00"}, "type": "virtual", "loop": "False", "outputs": {"CueOutputs": {"id": 3}}}}}}}, {"CuemsScript": {"uuid": "e05de59a-b281-4abf-83ba-97198d661a63", "name": "Segundo proyecto", "date": "13/08/2020 07:23:12", "timecode_cuelist": {"CueList": {"Cue": [{"uuid": "d47a75e2-f76e-4c77-b33e-e1df40ffdf02", "time": {"CTimecode": "00:00:33:00"}, "type": "mtc", "loop": "False", "outputs": {"CueOutputs": {"id": 5, "bla": "ble"}}}, {"uuid": "b5c35e3d-91f6-42d8-9825-0176354b44c1", "time": {"NoneType": "None"}, "type": "floating", "loop": "False", "outputs": {"CueOutputs": {"physiscal": 1, "virtual": 3}}}], "AudioCue": {"uuid": "aef5e289-03b0-4b39-99cd-90063d9b8c80", "time": {"CTimecode": "00:00:45:00"}, "type": "virtual", "loop": "True", "outputs": {"AudioCueOutputs": {"stereo": 1}}}}}, "floating_cuelist": {"CueList": {"DmxCue": {"uuid": "5d4ef443-5a49-4986-a283-9563ee7a9e85", "time": {"CTimecode": "00:00:23:00"}, "dmx_scene": {"DmxScene": {"DmxUniverse": [{"id": 0, "DmxChannel": [{"id": 0, "&": 10}, {"id": 1, "&": 50}]}, {"id": 1, "DmxChannel": [{"id": 20, "&": 23}, {"id": 21, "&": 255}]}, {"id": 2, "DmxChannel": [{"id": 5, "&": 10}, {"id": 6, "&": 23}, {"id": 7, "&": 125}, {"id": 8, "&": 200}]}]}}, "outputs": {"DmxCueOutputs": {"universe0": 3}}}, "Cue": {"uuid": "37f80125-1c41-4cce-aab1-13328dd8c94e", "time": {"CTimecode": "00:00:05:00"}, "type": "virtual", "loop": "False", "outputs": {"CueOutputs": {"id": 3}}}}}}}]
 
-    def __init__(self):
-        
-        self.event_loop = asyncio.new_event_loop()
-        #self.event_loop.set_exception_handler(self.exception_handler) ### TODO:UNCOMENT FOR PRODUCTION 
-        self.event = Event()
-        self.process = Process(target=self.run_async_server, args=(self.event,))
-        
 
     def start(self, port):
+        self.event = Event()
+        self.process = Process(target=self.run_async_server, args=(self.event,))
         self.port = port
         self.host = 'localhost'
         self.process.start()
@@ -36,7 +46,10 @@ class CuemsWsServer():
         
 
     def run_async_server(self, event):
+        self.event_loop = asyncio.new_event_loop()
         asyncio.set_event_loop(self.event_loop)
+        self.executor =  concurrent.futures.ThreadPoolExecutor(thread_name_prefix='ws_load_ThreadPoolExecutor', max_workers=3) # TODO: adjust max workers
+        #self.event_loop.set_exception_handler(self.exception_handler) ### TODO:UNCOMENT FOR PRODUCTION 
         self.project_server = ws.serve(self.connection_handler, self.host, self.port, max_size=None)
         for sig in (signal.SIGINT, signal.SIGTERM):
             self.event_loop.add_signal_handler(sig, self.ask_exit)
@@ -74,7 +87,7 @@ class CuemsWsServer():
             logging.info("unknow path: {}".format(path))
 
     async def project_manager_session(self, websocket):
-        user_session = CuemsWsUser(websocket)
+        user_session = CuemsWsUser(self, websocket)
         await self.register(user_session)
         await user_session.outgoing.put(self.counter_event())
         try:
@@ -92,7 +105,7 @@ class CuemsWsServer():
             await self.unregister(user_session)
 
     async def upload_session(self, websocket):
-        user_upload_session = CuemsUpload(websocket)
+        user_upload_session = CuemsUpload(self, websocket)
         logging.info("new upload session: {}".format(user_upload_session))
 
         await user_upload_session.message_handler()
@@ -154,21 +167,21 @@ class CuemsWsServer():
         logging.debug(context['message'])
 
 
-class CuemsWsUser(CuemsWsServer):
-    def __init__(self, websocket):
-        self.websocket = websocket
+class CuemsWsUser():
+    
+    def __init__(self, server, websocket):
+        self.server = server
+        asyncio.set_event_loop(server.event_loop)
         self.incoming = asyncio.Queue()
         self.outgoing = asyncio.Queue()
-        self.users[self] = None
-        self.event_loop = asyncio.get_running_loop()
-        self.executor =  concurrent.futures.ThreadPoolExecutor(thread_name_prefix='ws_load_ThreadPoolExecutor', max_workers=2) # TODO: move to server init and create 3 ?
-        
+        self.websocket = websocket
+        server.users[self] = None
 
     async def consumer_handler(self):
         try:
             async for message in self.websocket:
                 await self.incoming.put(message)
-        except ws.exceptions.ConnectionClosedError as e:
+        except (ws.exceptions.ConnectionClosed, ws.exceptions.ConnectionClosedOK, ws.exceptions.ConnectionClosedError) as e:
                 logging.debug(e)
 
     async def producer_handler(self):
@@ -189,11 +202,11 @@ class CuemsWsUser(CuemsWsServer):
                 logging.error("unsupported event: {}".format(data))
                 await self.notify_error_to_user("unsupported event: {}".format(data))
             elif data["action"] == "minus":
-                self.state["value"] -= 1
-                await self.notify_state()
+                self.server.state["value"] -= 1
+                await self.server.notify_state()
             elif data["action"] == "plus":
-                self.state["value"] += 1
-                await self.notify_state()
+                self.server.state["value"] += 1
+                await self.server.notify_state()
             elif data["action"] == "load":
                 await self.send_project(data["value"])
             elif data["action"] == "save":
@@ -225,7 +238,7 @@ class CuemsWsUser(CuemsWsServer):
     async def list_projects(self):
         logging.info("user {} loading project list".format(id(self.websocket)))
         try:
-            project_list = await self.event_loop.run_in_executor(self.executor, self.load_project_list)    
+            project_list = await self.server.event_loop.run_in_executor(self.server.executor, self.load_project_list)    
             await self.outgoing.put(json.dumps({"type": "list", "value": project_list}))
         except Exception as e:
             logging.debug("error loading project list")
@@ -237,11 +250,11 @@ class CuemsWsUser(CuemsWsServer):
             if project_uuid == '':
                 raise NameError
             logging.info("user {} loading project {}".format(id(self.websocket), project_uuid))
-            project = await self.event_loop.run_in_executor(self.executor, self.load_project, project_uuid)
+            project = await self.server.event_loop.run_in_executor(self.server.executor, self.load_project, project_uuid)
             msg = json.dumps({"type":"project", "value":json.dumps(project)})
             await self.outgoing.put(msg)
             await self.notify_user("project loaded")
-            self.users[self] = project_uuid
+            self.server.users[self] = project_uuid
         except Exception as e:
             logging.debug("error loading project")
             logging.error("error: {} {}".format(type(e), e))
@@ -254,10 +267,10 @@ class CuemsWsUser(CuemsWsServer):
 
             logging.info("user {} saving project {}".format(id(self.websocket), project_uuid))
             
-            return_message = await self.event_loop.run_in_executor(self.executor, self.save_project, project_uuid, project)
-            self.users[self] = project_uuid
+            return_message = await self.server.event_loop.run_in_executor(self.server.executor, self.save_project, project_uuid, project)
+            self.server.users[self] = project_uuid
             await self.notify_user("{} project saved".format(return_message))
-            await self.notify_others(self, "changes")
+            await self.server.notify_others(self, "changes")
         except Exception as e:
             logging.debug("error saving project")
             logging.error("error: {} {}".format(type(e), e))
@@ -267,11 +280,11 @@ class CuemsWsUser(CuemsWsServer):
         try:
             logging.info("user {} deleting project: {}".format(id(self.websocket), project_uuid))
             
-            if (await self.event_loop.run_in_executor(self.executor, self.delete_project,project_uuid)):
+            if (await self.server.event_loop.run_in_executor(self.server.executor, self.delete_project,project_uuid)):
 
                 # self.users[self] = None  #TODO:what is now the active project? deleted project was the active one?
                 await self.notify_user("project {} deleted".format(project_uuid))
-                await self.notify_others(self, "changes", project_uuid=project_uuid)
+                await self.server.notify_others(self, "changes", project_uuid=project_uuid)
         except Exception as e:
             logging.debug("error deleting project")
             logging.error("error: {} {}".format(type(e), e))
@@ -284,7 +297,7 @@ class CuemsWsUser(CuemsWsServer):
     def load_project_list(self):
         logging.info("loading project list")
         project_list = list()
-        for project in self.projects: #TODO: provisional
+        for project in self.server.projects: #TODO: provisional
             try:
                 project_list.append({project['CuemsScript']['uuid']:{"name":project['CuemsScript']['name'], "date":project['CuemsScript']['date']}})
             except:
@@ -294,7 +307,7 @@ class CuemsWsUser(CuemsWsServer):
     
     def load_project(self, project):
         logging.info("loading project: {}".format(project))
-        for elem in self.projects:
+        for elem in self.server.projects:
             if elem['CuemsScript']['uuid'] == project:
                 logging.debug("loading project: {}".format(elem))
                 return elem
@@ -303,35 +316,37 @@ class CuemsWsUser(CuemsWsServer):
     def save_project(self, uuid, data):
         logging.info("loading project: {}".format(uuid))
         logging.debug('saving project, uuid:{}, data:{}'.format(uuid, data))
-        for num, elem in enumerate(self.projects):
+        for num, elem in enumerate(self.server.projects):
             if elem['CuemsScript']['uuid'] == uuid:
-                self.projects[num] = data
+                self.server.projects[num] = data
                 return 'updated'
 
-        self.projects.append(data)
+        self.server.projects.append(data)
         return 'new'
 
     def delete_project(self, uuid):
         logging.info('deleting project, uuid:{}'.format(uuid))
-        for num, elem in enumerate(self.projects):
+        for num, elem in enumerate(self.server.projects):
             if elem['CuemsScript']['uuid'] == uuid:
-                del self.projects[num]
+                del self.server.projects[num]
                 return True
 
         raise NameError
 
 class CuemsUpload():
-    def __init__(self, websocket):
-        self.websocket = websocket
-        self.upload_forlder_path = os.path.join(os.getcwd(), 'upload')     #TODO: get folder path from settings or constant
-        self.uploading = False
-        self.filename = None
-        self.tmp_filename = None
-        self.bytes_received = 0
-        self.filesize = 0
-        self.file_handle = None
-        
 
+    uploading = False
+    filename = None
+    tmp_filename = None
+    bytes_received = 0
+    filesize = 0
+    file_handle = None
+    upload_forlder_path = os.path.join(os.getcwd(), 'upload')     #TODO: get folder path from settings or constant
+
+    def __init__(self, server, websocket):
+        self.server = server
+        self.websocket = websocket
+        
     async def message_handler(self):
         while True:
             try:
@@ -340,9 +355,15 @@ class CuemsUpload():
                     await self.process_upload_message(message)
                 elif isinstance(message, bytes):
                     await self.process_upload_packet(message)
-            except ws.exceptions.ConnectionClosedError:
+            except (ws.exceptions.ConnectionClosed, ws.exceptions.ConnectionClosedOK, ws.exceptions.ConnectionClosedError):
                 logging.debug('upload connection closed, exiting loop')
                 break
+
+    async def message_sender(self, message):
+        try:
+            await self.websocket.send(message)
+        except (ws.exceptions.ConnectionClosed, ws.exceptions.ConnectionClosedOK, ws.exceptions.ConnectionClosedError) as e:
+                logging.debug(e)
 
     async def process_upload_message(self, message):
         data = json.loads(message)
@@ -358,7 +379,7 @@ class CuemsUpload():
         
         if not os.path.exists(self.upload_forlder_path):
             logging.error("upload folder doenst exists")
-            await self.websocket.send(json.dumps({'error' : 'upload folder doenst exist', 'fatal': True}))
+            await self.message_sender(json.dumps({'error' : 'upload folder doenst exist', 'fatal': True}))
             return False
         
         self.filename = file_info['name']
@@ -370,9 +391,9 @@ class CuemsUpload():
             self.filesize = file_info['size']
             self.file_handle = open(tmp_path, 'wb')
             self.uploading = 'Ready'
-            await self.websocket.send(json.dumps({"ready" : True}))
+            await self.message_sender(json.dumps({"ready" : True}))
         else:
-            await self.websocket.send(json.dumps({'error' : 'file allready exists', 'fatal': True}))
+            await self.message_sender(json.dumps({'error' : 'file allready exists', 'fatal': True}))
             logging.error("file allready exists")
 
     async def process_upload_packet(self, bin_data):
@@ -380,7 +401,7 @@ class CuemsUpload():
         if self.uploading == 'Ready':
             self.file_handle.write(bin_data)
             self.bytes_received += len(bin_data)
-            await self.websocket.send(json.dumps({"ready" : True}))
+            await self.message_sender(json.dumps({"ready" : True}))
 
     async def upload_done(self):
         self.file_handle.close()
@@ -390,6 +411,7 @@ class CuemsUpload():
             i = 0
             while True:     
                 if not os.path.exists(dest_path):
+                    logging.info('new file uploaded, saving to: {}'.format(dest_path))
                     os.rename(tmp_path, dest_path)
                     break
                 else:
@@ -397,14 +419,13 @@ class CuemsUpload():
                     (base, ext) = os.path.splitext(self.filename)
                     increment_filename = base + str(i) + ext
                     dest_path = os.path.join(self.upload_forlder_path, increment_filename)
-                    print(dest_path)
                     continue
 
             logging.debug('upload completed')
-            await self.websocket.send(json.dumps({"close" : True}))
+            await self.message_sender(json.dumps({"close" : True}))
         except Exception as e:
             logging.error(e)
-            await self.websocket.send(json.dumps({'error' : 'error saving file', 'fatal': True}))
+            await self.message_sender(json.dumps({'error' : 'error saving file', 'fatal': True}))
         finally:
             self.uploading = False
             self.filename_path = None
