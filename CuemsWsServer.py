966043d22b4881a4d89e5ae8693a6e0b034e0c26
Ion Reguera <ion@stagelab.coop>
2025-06-20 12:09:27 +0200

    ipc addres without ipc:///

--- CuemsWsServer.py
+++ CuemsWsServer.py
@@ -27,7 +27,7 @@ from cuemsutils.CommunicatorServices import Communicator
 class CuemsWsServer():
     
     def __init__(self, settings_dict, mappings_dict ):
-        self.engine_communicator = Communicator(address="ipc:///tmp/test1.sock")  
+        self.engine_communicator = Communicator(address="/tmp/test1.sock")  
         #self.engine_queue = Comunicator(address="ipc:///tmp/test2.sock")
         self.engine_messages = list()
         self.users = dict()
