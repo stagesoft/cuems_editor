bc9580eefad7030781f71909106a175726ad0534
Ion Reguera <ion@stagelab.coop>
2025-08-04 11:22:02 +0200

    simplify poll executor calling functions

--- CuemsWsUser.py
+++ CuemsWsUser.py
@@ -88,23 +88,6 @@ class CuemsWsUser():
                 Logger.error("error: {} {}".format(type(e), e))
                 await self.notify_error_to_user('error processing request')
 
-
-    async def notify_user(self, msg=None, uuid=None,  action=None, new_uuid=None):
-        if (uuid is None) and (action is None) and (msg is not None):
-            await self.outgoing.put(json.dumps({"type": "state", "value":msg}))
-        elif (msg is None and new_uuid is None):
-            await self.outgoing.put(json.dumps({"type": action, "value": uuid}))
-        elif (msg is None and new_uuid is not None):
-            await self.outgoing.put(json.dumps({"type": action, "value": { "uuid" : uuid, "new_uuid" : new_uuid}}))
-
-    async def notify_error_to_user(self, msg=None, uuid=None, action=None):
-        if (msg is not None) and (uuid is None) and (action is None):
-            await self.outgoing.put(json.dumps({"type": "error", "value": msg}))
-        elif (action is not None) and (msg is not None) and (uuid is None):
-            await self.outgoing.put(json.dumps({"type": "error", "action": action, "value": msg}))
-        elif (action is not None) and (msg is not None) and (uuid is not None):
-            await self.outgoing.put(json.dumps({"type": "error", "uuid": uuid, "action": action, "value": msg}))
-
     async def comunicate_with_engine(self, action, action_uuid, engine_command):
         try:
             try:
@@ -131,12 +114,27 @@ class CuemsWsUser():
         except Exception as e:
             raise e
 
-        
+    async def notify_user(self, msg=None, uuid=None,  action=None, new_uuid=None):
+        if (uuid is None) and (action is None) and (msg is not None):
+            await self.outgoing.put(json.dumps({"type": "state", "value":msg}))
+        elif (msg is None and new_uuid is None):
+            await self.outgoing.put(json.dumps({"type": action, "value": uuid}))
+        elif (msg is None and new_uuid is not None):
+            await self.outgoing.put(json.dumps({"type": action, "value": { "uuid" : uuid, "new_uuid" : new_uuid}}))
+
+    async def notify_error_to_user(self, msg=None, uuid=None, action=None):
+        if (msg is not None) and (uuid is None) and (action is None):
+            await self.outgoing.put(json.dumps({"type": "error", "value": msg}))
+        elif (action is not None) and (msg is not None) and (uuid is None):
+            await self.outgoing.put(json.dumps({"type": "error", "action": action, "value": msg}))
+        elif (action is not None) and (msg is not None) and (uuid is not None):
+            await self.outgoing.put(json.dumps({"type": "error", "uuid": uuid, "action": action, "value": msg}))
+
 
     async def project_ready(self, project_uuid, action):
         Logger.info(f"user {id(self.websocket)} requesting ready project {project_uuid}")
         try:
-            unix_name = await self.server.event_loop.run_in_executor(self.server.executor, self.get_project_unix_name, project_uuid)
+            unix_name = await self.server.event_loop.run_in_executor(self.server.executor, self.server.db.project.get_project_unix_name, project_uuid)
             action_uuid = str(new_uuid())
             engine_command = {"action" : "project_ready", "action_uuid": action_uuid, "value" : unix_name}
 
@@ -165,7 +163,7 @@ class CuemsWsUser():
     async def project_deploy(self, project_uuid, action):
         Logger.info(f"user {id(self.websocket)} requesting deploy project {project_uuid}")
         try:
-            unix_name = await self.server.event_loop.run_in_executor(self.server.executor, self.get_project_unix_name, project_uuid)
+            unix_name = await self.server.event_loop.run_in_executor(self.server.executor, self.server.db.project.get_project_unix_name, project_uuid)
             action_uuid = str(new_uuid())
             engine_command = {"action" : "project_deploy", "action_uuid": action_uuid, "value" : unix_name}
 
@@ -180,7 +178,7 @@ class CuemsWsUser():
     async def list_project(self, action):
         Logger.info("user {} loading project list".format(id(self.websocket)))
         try:
-            project_list = await self.server.event_loop.run_in_executor(self.server.executor, self.load_project_list)    
+            project_list = await self.server.event_loop.run_in_executor(self.server.executor, self.server.db.project.list)    
             await self.outgoing.put(json.dumps({"type": action, "value": project_list}))
         except Exception as e:
             Logger.error("error: {} {}".format(type(e), e))
@@ -190,7 +188,7 @@ class CuemsWsUser():
     async def send_project(self, project_uuid, action):
         try:
             Logger.info("user {} loading project {}".format(id(self.websocket), project_uuid))
-            project = await self.server.event_loop.run_in_executor(self.server.executor, self.load_project, project_uuid)
+            project = await self.server.event_loop.run_in_executor(self.server.executor, self.server.db.project.load, project_uuid)
             msg = json.dumps({"type":"project", "value":project})
             await self.outgoing.put(msg)
             self.server.users[self] = project_uuid
@@ -206,7 +204,7 @@ class CuemsWsUser():
     async def received_new_project(self, data, action, unix_name):
         try:
 
-            project_uuid = await self.server.event_loop.run_in_executor(self.server.executor, self.new_project, data, unix_name)
+            project_uuid = await self.server.event_loop.run_in_executor(self.server.executor, self.server.db.project.new, data, unix_name)
             
             Logger.info("user {} new project {}".format(id(self.websocket), project_uuid))
             
@@ -221,7 +219,7 @@ class CuemsWsUser():
         try:
 
             project_uuid = data['CuemsScript']['id']
-            await self.server.event_loop.run_in_executor(self.server.executor, self.update_project, project_uuid, data)
+            await self.server.event_loop.run_in_executor(self.server.executor, self.server.db.project.update, project_uuid, data)
             Logger.info("user {} saving project {}".format(id(self.websocket), project_uuid))
             
             
@@ -236,7 +234,7 @@ class CuemsWsUser():
     async def list_project_trash(self, action):
         Logger.info("user {} loading project trash list".format(id(self.websocket)))
         try:
-            project_trash_list = await self.server.event_loop.run_in_executor(self.server.executor, self.load_project_trash_list)    
+            project_trash_list = await self.server.event_loop.run_in_executor(self.server.executor, self.server.db.project.list_trash)    
             await self.outgoing.put(json.dumps({"type": action, "value": project_trash_list}))
         except Exception as e:
             Logger.error("error: {} {}".format(type(e), e))
@@ -246,7 +244,7 @@ class CuemsWsUser():
         try:
             Logger.info("user {} deleting project: {}".format(id(self.websocket), project_uuid))
             
-            await self.server.event_loop.run_in_executor(self.server.executor, self.delete_project, project_uuid)
+            await self.server.event_loop.run_in_executor(self.server.executor, self.server.db.project.delete, project_uuid)
 
             await self.notify_user(uuid=project_uuid, action=action)
             await self.server.notify_others_same_project(self, "project_update", project_uuid=project_uuid)
@@ -262,7 +260,7 @@ class CuemsWsUser():
     async def request_duplicate_project(self, project_uuid, action):
         try:
             Logger.info("user {} duplicating project: {}".format(id(self.websocket), project_uuid))
-            new_project_uuid = await self.server.event_loop.run_in_executor(self.server.executor, self.duplicate_project, project_uuid)
+            new_project_uuid = await self.server.event_loop.run_in_executor(self.server.executor, self.server.db.project.duplicate, project_uuid)
             await self.notify_user(uuid=project_uuid, action=action, new_uuid=new_project_uuid)
             await self.server.notify_others_list_changes(self, "project_list")
             await self.server.notify_others_list_changes(self, "file_list")
@@ -276,7 +274,7 @@ class CuemsWsUser():
     async def request_restore_project(self, project_uuid, action):
         try:
             Logger.info("user {} restoring project: {}".format(id(self.websocket), project_uuid))
-            await self.server.event_loop.run_in_executor(self.server.executor, self.restore_project, project_uuid)
+            await self.server.event_loop.run_in_executor(self.server.executor, self.server.db.project.restore, project_uuid)
             await self.notify_user(uuid=project_uuid, action=action)
             await self.server.notify_others_list_changes(self, "project_list")
             await self.server.notify_others_list_changes(self, "project_trash_list")
@@ -291,7 +289,7 @@ class CuemsWsUser():
         try:
             Logger.info("user {} deleting project from trash: {}".format(id(self.websocket), project_uuid))
             
-            await self.server.event_loop.run_in_executor(self.server.executor, self.delete_project_trash, project_uuid)
+            await self.server.event_loop.run_in_executor(self.server.executor, self.server.db.project.delete_from_trash, project_uuid)
 
             await self.notify_user(uuid=project_uuid, action=action)
             await self.server.notify_others_list_changes(self, "project_trash_list")
@@ -305,7 +303,7 @@ class CuemsWsUser():
     async def list_file(self, action):
         Logger.info("user {} loading file list".format(id(self.websocket)))
         try:
-            file_list = await self.server.event_loop.run_in_executor(self.server.executor, self.load_file_list)    
+            file_list = await self.server.event_loop.run_in_executor(self.server.executor, self.server.db.media.list)    
             await self.outgoing.put(json.dumps({"type": action, "value": file_list}))
         except Exception as e:
             Logger.error("error: {} {}".format(type(e), e))
@@ -317,7 +315,7 @@ class CuemsWsUser():
 
             Logger.info("user {} update file data {}".format(id(self.websocket), file_uuid))
             
-            return_message = await self.server.event_loop.run_in_executor(self.server.executor, self.save_file, file_uuid, data)
+            return_message = await self.server.event_loop.run_in_executor(self.server.executor, self.server.db.media.save, file_uuid, data)
             await self.notify_user(uuid=file_uuid, action=action)
         except Exception as e:
             Logger.error("error: {} {}".format(type(e), e))
@@ -328,7 +326,7 @@ class CuemsWsUser():
 
             Logger.info("user {} loading file meta data {}".format(id(self.websocket), file_uuid))
             
-            file_meta_data = await self.server.event_loop.run_in_executor(self.server.executor, self.load_file_meta, file_uuid)
+            file_meta_data = await self.server.event_loop.run_in_executor(self.server.executor, self.server.db.media.load_meta, file_uuid)
             await self.outgoing.put(json.dumps({"type": action, "value": file_meta_data}))
         except NonExistentItemError as e:
             Logger.info(e)
@@ -342,7 +340,7 @@ class CuemsWsUser():
 
             Logger.info("user {} loading file thumbnail {}".format(id(self.websocket), file_uuid))
             
-            file_thumbnail = await self.server.event_loop.run_in_executor(self.server.executor, self.load_file_thumbnail, file_uuid)
+            file_thumbnail = await self.server.event_loop.run_in_executor(self.server.executor, self.server.db.media.load_thumbnail, file_uuid)
             await self.outgoing.put(file_thumbnail) #TODO: add uuid encoded in the binary message
         except NonExistentItemError as e:
             Logger.warning(e)
@@ -356,7 +354,7 @@ class CuemsWsUser():
 
             Logger.info("user {} loading file waveform {}".format(id(self.websocket), file_uuid))
             
-            file_waveform = await self.server.event_loop.run_in_executor(self.server.executor, self.load_file_waveform, file_uuid)
+            file_waveform = await self.server.event_loop.run_in_executor(self.server.executor, self.server.db.media.load_waveform, file_uuid)
             await self.outgoing.put(file_waveform) #TODO: add uuid encoded in the binary message
         except NonExistentItemError as e:
             Logger.warning(e)
@@ -368,7 +366,7 @@ class CuemsWsUser():
     async def list_file_trash(self, action):
         Logger.info("user {} loading file trash list".format(id(self.websocket)))
         try:
-            file_trash_list = await self.server.event_loop.run_in_executor(self.server.executor, self.load_file_trash_list)    
+            file_trash_list = await self.server.event_loop.run_in_executor(self.server.executor, self.server.db.media.list_trash)    
             await self.outgoing.put(json.dumps({"type": action, "value": file_trash_list}))
         except Exception as e:
             Logger.error("error: {} {}".format(type(e), e))
@@ -378,7 +376,7 @@ class CuemsWsUser():
     async def request_delete_file(self, file_uuid, action):
         try:
             Logger.debug("user {} deleting file: {}".format(id(self.websocket), file_uuid))
-            await self.server.event_loop.run_in_executor(self.server.executor, self.delete_file, file_uuid)
+            await self.server.event_loop.run_in_executor(self.server.executor, self.server.db.media.delete, file_uuid)
             await self.notify_user(uuid=file_uuid, action=action)
             await self.server.notify_others_list_changes(self, "file_list")
             await self.server.notify_others_list_changes(self, "file_trash_list")
@@ -392,7 +390,7 @@ class CuemsWsUser():
     async def request_restore_file(self, file_uuid, action):
         try:
             Logger.debug("user {} restoring file: {}".format(id(self.websocket), file_uuid))
-            await self.server.event_loop.run_in_executor(self.server.executor, self.restore_file, file_uuid)
+            await self.server.event_loop.run_in_executor(self.server.executor, self.server.db.media.restore, file_uuid)
             await self.notify_user(uuid=file_uuid, action=action)
             await self.server.notify_others_list_changes(self, "file_list")
             await self.server.notify_others_list_changes(self, "file_trash_list")
@@ -406,7 +404,7 @@ class CuemsWsUser():
     async def request_delete_file_trash(self, file_uuid, action):
         try:
             Logger.info("user {} deleting file from trash: {}".format(id(self.websocket), file_uuid))
-            await self.server.event_loop.run_in_executor(self.server.executor, self.delete_file_trash, file_uuid)
+            await self.server.event_loop.run_in_executor(self.server.executor, self.server.db.media.delete_from_trash, file_uuid)
             await self.notify_user(uuid=file_uuid, action=action)
             await self.server.notify_others_list_changes(self, "file_trash_list")
         except NonExistentItemError as e:
@@ -415,76 +413,3 @@ class CuemsWsUser():
         except Exception as e:
             Logger.error("error: {} {}".format(type(e), e))
             await self.notify_error_to_user(str(e), uuid=file_uuid, action=action)
-
-    # call blocking functions asynchronously with run_in_executor ThreadPoolExecutor
-    def load_project_list(self):
-        Logger.info("loading project list")
-        return self.server.db.project.list()
-
-    def get_project_unix_name(self, project_uuid):
-        Logger.info("loading project unix_name")
-        return self.server.db.project.get_project_unix_name(project_uuid)
-
-    
-    def load_project(self, project_uuid):
-        Logger.info("loading project: {}".format(project_uuid))
-        return self.server.db.project.load(project_uuid)
-
-    def new_project(self, data, unix_name):
-        Logger.debug('saving new project, data:{}, unix_name{}'.format(data, unix_name))
-        return self.server.db.project.new(data, unix_name)
-
-    def update_project(self, project_uuid, data):
-        Logger.debug('saving project, uuid:{}, data:{}'.format(project_uuid, data))
-        self.server.db.project.update(project_uuid, data)
-
-    def duplicate_project(self, project_uuid):
-        Logger.debug('duplicating project, uuid:{}'.format(project_uuid))
-        return self.server.db.project.duplicate(project_uuid)
-
-    def delete_project(self, project_uuid):
-        self.server.db.project.delete(project_uuid)
-
-    def restore_project(self, project_uuid):
-        self.server.db.project.restore(project_uuid)
-
-    def load_project_trash_list(self):
-        Logger.info("loading project trash list")
-        return self.server.db.project.list_trash()
-
-    def delete_project_trash(self, project_uuid):
-        self.server.db.project.delete_from_trash(project_uuid)
-
-    def load_file_list(self):
-        Logger.info("loading file list")
-        return self.server.db.media.list()
-
-    def load_file_meta(self, uuid):
-        Logger.info("loading file meta")
-        return self.server.db.media.load_meta(uuid)
-        
-    def load_file_thumbnail(self, uuid):
-        Logger.info("loading file thumbnail")
-        return self.server.db.media.load_thumbnail(uuid)
-
-    def load_file_waveform(self, uuid):
-        Logger.info("loading file waveform")
-        return self.server.db.media.load_waveform(uuid)
-
-
-    def save_file(self, file_uuid, data):
-        Logger.info("saving file data")
-        self.server.db.media.save(file_uuid, data)
-
-    def delete_file(self, file_uuid):
-        self.server.db.media.delete(file_uuid)
-
-    def restore_file(self, file_uuid):
-        self.server.db.media.restore(file_uuid)
-
-    def load_file_trash_list(self):
-        Logger.info("loading file trash list")
-        return self.server.db.media.list_trash()
-
-    def delete_file_trash(self, file_uuid):
-        self.server.db.media.delete_from_trash(file_uuid)
