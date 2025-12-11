ed5d8c6bdc8370498b412245abd9300e58bad2e6
Ion Reguera <ion@stagelab.coop>
2025-12-11 13:45:22 +0100

    Update package: fix copyright, simplify __init__.py, update version and dependencies
    - Update copyright to Stagelab coop and Ion Reguera <ion@stagelab.coop>
    - Simplify __init__.py to match cuems-utils style (minimal, SPDX format)
    - Remove package-level exports (use direct imports)
    - Update version to 0.1.0rc1
    - Update dependencies to cuems-utils >= 0.1.0rc4

--- src/cuemseditor/__init__.py
+++ src/cuemseditor/__init__.py
@@ -1,19 +1,4 @@
-"""
-CUEMS Editor - Collaborative Universal Edit Management System
-
-WebSocket-based server for collaborative editing and project management.
-"""
-
-__version__ = "0.1.0"
-__author__ = "CUEMS Team"
-__license__ = "GPL-3.0"
-
-# Package exports
-from cuemseditor.CuemsWsServer import CuemsWsServer
-from cuemseditor.CuemsWsUser import CuemsWsUser
-from cuemseditor.CuemsErrors import *
-
-__all__ = [
-    "CuemsWsServer",
-    "CuemsWsUser",
-]
+# SPDX-FileCopyrightText: 2025-present Stagelab coop and Ion Reguera <ion@stagelab.coop>
+#
+# SPDX-License-Identifier: GPL-3.0
+__version__ = "0.1.0rc1"
